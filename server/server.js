629f65fbd6d57bd39236bcd25f977317f335c572
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2023-01-20 23:30:32 +0800

    post to ping

--- server/server.js
+++ server/server.js
@@ -6,6 +6,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.post('/', (req, res) => {
+  let text = req.body.string;
+  res.send(`Received string: ${text}}`)
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
