05d47cbbf736230b15a56d6a1867159d9af626cb
Bekzat Baubek <39556077+bekzatbaubek@users.noreply.github.com>
2023-01-22 00:14:13 +0800

    backend working!

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,90 +1,40 @@
 import React, { useState } from 'react';
-import styles from './Home.module.css';
-
- const data = [
-  {
-    title : "Lasagna",
-    description : "This is an italian recipe born in the Milan and now appreciated and cooked in the whole world",
-    ingredients: [ "Lasagna sheets", "Tomato", "Basil", "minced-beef", "onion" ]
-  },
-  {
-    title : "Pizza",
-    description : "This is a margherrita style pizza",
-    ingredients: [ "Floor", "Tomatoes", "Mozzarella", "Olive oil" ]
-  },
-  {
-    title : "Beef Burger",
-    description : "This is an american style burger with ground beef patty and freshly baked bread",
-    ingredients: [ "minced beef", "Onion", "lettuce", "cucumber" ]
-  },
-  {
-    title : "Noodle soup",
-    description : "This is a Hong Kong street food",
-    ingredients: [ "Egg noodles", "Fish balls", "lettuce" ]
-  },
-  {
-    title : "BBQ pork with rice",
-    description : "This is a Hong KOng style traditional meal",
-    ingredients: [ "BBQ Pork", "Rice" ]
-  },
-];
 
 
 
+export default function Home () {
+  const [ string, setString ] = useState("");
 
-export default function Home() {
-
-  const [text, setText] = useState("");
-  const [recipes] = useState(data);
 
   return (
     <div>
-      <input type="text" 
+      <input type="text"
         onChange={ (event) => {
-          setText(event.target.value)
+          setString(event.target.value);
         }
         }
       />
 
-      <button 
-        onClick={() => {
+      <button
+        onClick={ () => {
           fetch('http://localhost:3001', {
             method: 'POST',
-            mode: 'cors', 
-            cache: 'no-cache', 
+            mode: 'cors',
+            cache: 'no-cache',
             credentials: 'same-origin',
             headers: {
               'Content-Type': 'application/json'
             },
-            body: JSON.stringify(text)
-          
-          })
-            
+            body: JSON.stringify(string)
 
-        }}
+          });
+
+
+        } }
       >
         Add String
       </button>
 
-      <div className={styles.recipesContainer}>
-        {recipes.map((recipe) => {
-          return(
-            <div key={recipe} className={styles.container}>
-              <h3>{recipe.title}</h3>
-              <p>{recipe.description}</p>
-
-              <ul className={styles.tagContainer}>
-                {recipe.ingredients.map((ingre) => {
-                  return(
-                    <li key={ingre} className={styles.tags}>{ingre}</li>
-                  )
-                })}
-              </ul>
-          </div>
-          )
-        
-        })}
-      </div>
     </div>
-  )
+  );
 }
--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from "cors";
 const app = express();
 const port = 3001;
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true,
+};
+
+app.options('/', cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/', (req, res) => {
-  let text = req.body.string;
-  res.send(`Received string: ${text}}`)
+app.post('/', cors(corsOptions), (req, res) => {
+  let txt = req.body;
+  console.log(`${req.method} on "/": ${txt}`);
+  res.send({ 'test': 'test' });
 });
 
 app.listen(port, () => {
